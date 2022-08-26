adf76280a815c5d91fda5b5424006ca72e4c647f
AllanMwangii <ayubgmwangi@gmail.com>
2022-08-26 10:43:41 +0300

    first commit

---
+++ app/src/main/java/com/example/foodapp/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.foodapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+}
