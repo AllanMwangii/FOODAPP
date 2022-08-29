95911fa54bd112609d82c7e8c8dad12360e43cfa
AllanMwangii <ayubgmwangi@gmail.com>
2022-08-29 11:14:51 +0300

    HELLO WORLD

--- app/src/main/java/com/example/foodapp/HomeActivity.kt
+++ app/src/main/java/com/example/foodapp/HomeActivity.kt
@@ -2,7 +2,7 @@ package com.example.foodapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/foodapp/IntroActivity.kt
+++ app/src/main/java/com/example/foodapp/IntroActivity.kt
@@ -4,17 +4,20 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
-class IntroActivity : AppCompatActivity() {
+class   IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
 
-        Handler().postDelayed({
-                              val intent = Intent(this@IntroActivity,HomeActivity::class.java)
+        val handler= Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this@IntroActivity, HomeActivity::class.java)
             startActivity(intent)
+            finish()
         }, 3000)
 
     }
